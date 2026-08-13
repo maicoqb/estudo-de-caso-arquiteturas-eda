374b1a6b66caef6c460b4955701bd35e6c464227
Maico Borges <maicoqb@gmail.com>
2026-08-13 11:54:48 -0300

    feat(base): order api database connection

--- apps/base/order-api/src/app/app.module.ts
+++ apps/base/order-api/src/app/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { HealthController } from './health.controller';
+import { Order } from './orders/order.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: parseInt(process.env.POSTGRES_PORT || '5432'),
+      username: process.env.POSTGRES_USER || 'admin',
+      password: process.env.POSTGRES_PASSWORD || 'admin',
+      database: process.env.POSTGRES_DB || 'eda',
+      entities: [Order],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Order]),
+  ],
   controllers: [HealthController],
   providers: [],
 })
